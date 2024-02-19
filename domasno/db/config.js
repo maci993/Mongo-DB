06ab647752a96ace92c76a0f29874e955757304c
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2024-02-19 09:27:38 +0100

    Update config.js

--- domasno/db/config.js
+++ domasno/db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = `mongodb+srv://savicmarina993:macencedoll993@cluster0.6ic7awf.mongodb.net/Semos?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://savicmarina993:<password>@cluster0.6ic7awf.mongodb.net/Semos?retryWrites=true&w=majority`;
 
 async function connect() {
   try {
